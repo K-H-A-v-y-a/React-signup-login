a6ebf6640e6423ec9d4e088d12d997cd43312ee8
Kavya <kavyardj9876@gmail.com>
2023-09-17 22:41:40 +0530

    router editted

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Profile from './components/Profile';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/sign" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/React-signup-login/" element={<Login />} />
+        <Route path="/React-signup-login/sign" element={<Signup />} />
+        <Route path="/React-signup-login/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
